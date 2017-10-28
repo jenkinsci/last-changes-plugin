fea9d79b3b156aa6b384f92c1247e2d33b80a2e5
rmpestano <rmpestano@gmail.com>
2017-10-28 12:05:57 -0200

    fixes #14

--- src/main/java/com/github/jenkins/lastchanges/model/LastChangesConfig.java
+++ src/main/java/com/github/jenkins/lastchanges/model/LastChangesConfig.java
@@ -1,81 +1,94 @@
 package com.github.jenkins.lastchanges.model;
 
 public class LastChangesConfig {
-	
-	private FormatType format = FormatType.LINE;
-	private MatchingType matching = MatchingType.NONE;
-	private String matchWordsThreshold = "0.25";
-	private String matchingMaxComparisons = "1000";
-	private Boolean showFiles = Boolean.TRUE;
-	private Boolean synchronisedScroll = Boolean.TRUE;
-	private String endRevision ="";//by default it is previous repository revision
-	
-	
-	
-	public LastChangesConfig() {
-	}
 
-	public LastChangesConfig(String endRevision, FormatType format, MatchingType matching, Boolean showFiles, Boolean synchronisedScroll, String matchWordsThreshold, String matchingMaxComparisons) {
-		super();
+    private FormatType format = FormatType.LINE;
+    private MatchingType matching = MatchingType.NONE;
+    private String matchWordsThreshold = "0.25";
+    private String matchingMaxComparisons = "1000";
+    private Boolean showFiles = Boolean.TRUE;
+    private Boolean synchronisedScroll = Boolean.TRUE;
+    private String endRevision = "";//by default it is previous repository revision
+    private Boolean lastSuccessFulBuild = Boolean.FALSE;
 
-		if(endRevision != null) {
-			this.endRevision = endRevision;
-		}
-		if(format != null){
-			this.format = format;
-		}
-		if(matching != null){
-			this.matching = matching;
-		}
-		if(showFiles != null){
-			this.showFiles = showFiles;
-		}
-		if(synchronisedScroll != null){
-			this.synchronisedScroll = synchronisedScroll;
-		}
-		if(matchingMaxComparisons != null){
-			try{
-				this.matchingMaxComparisons = String.valueOf(Double.parseDouble(matchingMaxComparisons));
-			}catch(NumberFormatException e){
-				//invalid number stay with default
-			}
-		}
-		
-		if(matchWordsThreshold != null){
-			try{
-				this.matchWordsThreshold = String.valueOf(Integer.parseInt(matchWordsThreshold));
-			}catch(NumberFormatException e){
-				//invalid number stay with default
-			}
-		}
-		
-	}
-	
-	public FormatType format() {
-		return format;
-	}
 
-	public String getEndRevision() {
-		return endRevision;
-	}
+    public LastChangesConfig() {
+    }
 
-	public MatchingType matching() {
-		return matching;
-	}
+    public LastChangesConfig(String endRevision, Boolean lastSuccessFulBuild, FormatType format, MatchingType matching, Boolean showFiles, Boolean synchronisedScroll, String matchWordsThreshold, String matchingMaxComparisons) {
+        super();
 
-	public String showFiles(){
-		return showFiles.toString();
-	}
-	
-	public String synchronisedScroll(){
-		return synchronisedScroll.toString();
-	}
+        if (endRevision != null) {
+            this.endRevision = endRevision;
+        }
 
-	public String matchingMaxComparisons(){
-		return matchingMaxComparisons;
-	}
-	
-	public String matchWordsThreshold(){
-		return matchWordsThreshold;
-	}
+        if (lastSuccessFulBuild != null) {
+            this.lastSuccessFulBuild = lastSuccessFulBuild;
+        }
+
+        if (format != null) {
+            this.format = format;
+        }
+
+        if (matching != null) {
+            this.matching = matching;
+        }
+
+        if (showFiles != null) {
+            this.showFiles = showFiles;
+        }
+
+        if (synchronisedScroll != null) {
+            this.synchronisedScroll = synchronisedScroll;
+        }
+
+        if (matchingMaxComparisons != null) {
+            try {
+                this.matchingMaxComparisons = String.valueOf(Double.parseDouble(matchingMaxComparisons));
+            } catch (NumberFormatException e) {
+                //invalid number stay with default
+            }
+        }
+
+        if (matchWordsThreshold != null) {
+            try {
+                this.matchWordsThreshold = String.valueOf(Integer.parseInt(matchWordsThreshold));
+            } catch (NumberFormatException e) {
+                //invalid number stay with default
+            }
+        }
+
+    }
+
+    public FormatType format() {
+        return format;
+    }
+
+    public String getEndRevision() {
+        return endRevision;
+    }
+
+    public MatchingType matching() {
+        return matching;
+    }
+
+    public String showFiles() {
+        return showFiles.toString();
+    }
+
+    public String synchronisedScroll() {
+        return synchronisedScroll.toString();
+    }
+
+    public String matchingMaxComparisons() {
+        return matchingMaxComparisons;
+    }
+
+    public String matchWordsThreshold() {
+        return matchWordsThreshold;
+    }
+
+    public Boolean lastSuccessFulBuild() {
+         return lastSuccessFulBuild;
+    }
 }
