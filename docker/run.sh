4a9dcd7f6422959ab0e612af0fe40920e48d1cbc
rmpestano <rmpestano@gmail.com>
2019-04-06 09:21:53 -0300

    update docker image

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --name jenkins-last-changes -p 8080:8080 -v /var/jenkins_home rmpestano/jenkins-last-changes
+docker run -it --rm --name jenkins-last-changes -p 8080:8080 -v /var/jenkins_home rmpestano/jenkins-last-changes
